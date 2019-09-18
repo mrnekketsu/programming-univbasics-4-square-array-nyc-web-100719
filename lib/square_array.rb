772a48e150a47461841d55e294f1813ed8089422
Nick <nick.moi@gmail.com>
2019-09-18 00:43:04 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
   # your code here
+  counter = 0
+  while counter < array.length do
+    value = array[counter]
+    array[counter] = array[counter] ** 2
+    counter += 1
+  end
+  array
 end
